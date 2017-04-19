e4d1d0c7bd5e7b3da9f9f633def6d04106701da6
Trevor Ross <trevor@tjrcadd.com>
2017-04-19 18:32:06 -0400

    add header footer and lobby components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LobbyComponent } from './lobby/lobby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LobbyComponent
   ],
   imports: [
     BrowserModule,
